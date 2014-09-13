eb7baed6aa92fcec0b4dc9a8832898ecc90aaadf
Merlyn <curious.attempt.bunny@gmail.com>
2014-09-13 11:25:22 -0700

    Handle head requests.

--- src/org/example/server.clj
+++ src/org/example/server.clj
@@ -1,13 +1,15 @@
 (ns org.example.server
   (:require
     [ring.adapter.jetty :as jetty]
-    [io.aviso.rook :as rook]))
+    [io.aviso.rook :as rook]
+    [ring.middleware.head :as head]))
 
 (defn start-server
 	[port]
   (let [handler (-> (rook/namespace-handler
                      [["counters"] 'org.example.resources.counters])
-	                   rook/wrap-with-standard-middleware)]
+	                   rook/wrap-with-standard-middleware
+	                   head/wrap-head)]
      (jetty/run-jetty handler {:port port :join? false})))
 
 (defn main [] (start-server 8080))
